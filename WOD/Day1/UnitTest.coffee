8e864784c145ba635983c31872ffdc70ea2cbe01
HackerWorkout <workouthacker@gmail.com>
2013-12-26 20:49:26 -0800

    Finished Day 1 WOD
    I found this to be a good exercise for starting CoffeeScript. It
    required me to go back a couple times to the website and really
    understand what I was doing.

---
+++ WOD/Day1/UnitTest.coffee
@@ -0,0 +1,32 @@
+﻿# CoffeeScript
+
+class AssertBase
+    Fail: (msg) -> throw msg
+
+    Equal: (expected, actual) -> if expected != actual then Assert.Fail "#{expected} != #{actual}"
+
+    True: (actual) -> Assert.Equal(true, actual)
+
+    Exception: (func) -> 
+        try
+            func
+            Assert.Fail "Expected failure did not occur."
+        catch error
+
+Assert = new AssertBase
+
+Test = (name, testfunc) -> 
+    try
+        testfunc()
+        console.log "#{name} passed."
+    catch error
+        console.log "#{name} failed with #{error}"
+
+
+Test "Basic Test", () -> true
+
+Test "Addition Test", () -> Assert.Equal(4, 2 + 2)
+
+Test "ExpectedFailure", () -> Assert.Exception( () -> throw "failure" )
+
+Test "FailedTest", () -> Assert.True(false)
---
+++ WOD/Day1/UnitTest.js
@@ -0,0 +1,68 @@
+﻿(function() {
+  var Assert, AssertBase, Test;
+
+
+
+  AssertBase = (function() {
+    function AssertBase() {}
+
+    AssertBase.prototype.Fail = function(msg) {
+      throw msg;
+    };
+
+    AssertBase.prototype.Equal = function(expected, actual) {
+      if (expected !== actual) {
+        return Assert.Fail("" + expected + " != " + actual);
+      }
+    };
+
+    AssertBase.prototype.True = function(actual) {
+      return Assert.Equal(true, actual);
+    };
+
+    AssertBase.prototype.Exception = function(func) {
+      var error;
+      try {
+        func;
+        return Assert.Fail("Expected failure did not occur.");
+      } catch (_error) {
+        error = _error;
+      }
+    };
+
+    return AssertBase;
+
+  })();
+
+  Assert = new AssertBase;
+
+  Test = function(name, testfunc) {
+    var error;
+    try {
+      testfunc();
+      return console.log("" + name + " passed.");
+    } catch (_error) {
+      error = _error;
+      return console.log("" + name + " failed with " + error);
+    }
+  };
+
+  Test("Basic Test", function() {
+    return true;
+  });
+
+  Test("Addition Test", function() {
+    return Assert.Equal(4, 2 + 2);
+  });
+
+  Test("ExpectedFailure", function() {
+    return Assert.Exception(function() {
+      throw "failure";
+    });
+  });
+
+  Test("FailedTest", function() {
+    return Assert.True(false);
+  });
+
+}).call(this);
